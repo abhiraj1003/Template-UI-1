094c0da671788aa6f58eb969f8e67f62c832b174
abhiraj1003 <abhiraj007lol@gmail.com>
2024-07-04 13:05:38 +0000

    changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from '/src/Logos/Navbar/connecting dot erp logo.png';
-import { Navbar, Container, Offcanvas, Nav, Button } from 'react-bootstrap';
+import { Navbar, Container, Offcanvas, Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './Navbar.css';
@@ -11,6 +11,18 @@ const Header = () => (
       <div className="logo">
         <img src={logo} alt="Connecting Dots ERP" />
       </div>
+      <form action="" className="search">
+        <input className="search__input" type="search" placeholder="Search" id="searchInput" />
+        <div className="search__icon-container">
+          <label htmlFor="searchInput" className="search__label" aria-label="Search">
+            <svg viewBox="0 0 1000 1000" title="Search"><path fill="currentColor" d="M408 745a337 337 0 1 0 0-674 337 337 0 0 0 0 674zm239-19a396 396 0 0 1-239 80 398 398 0 1 1 319-159l247 248a56 56 0 0 1 0 79 56 56 0 0 1-79 0L647 726z"/></svg>
+          </label>
+          <button className="search__submit" aria-label="Search">
+            <svg viewBox="0 0 1000 1000" title="Search"><path fill="currentColor" d="M408 745a337 337 0 1 0 0-674 337 337 0 0 0 0 674zm239-19a396 396 0 0 1-239 80 398 398 0 1 1 319-159l247 248a56 56 0 0 1 0 79 56 56 0 0 1-79 0L647 726z"/></svg>
+          </button>
+        </div>
+      </form>
+
       <Navbar.Toggle aria-controls="offcanvasNavbar" />
       <Navbar.Offcanvas
         id="offcanvasNavbar"
@@ -21,14 +33,129 @@ const Header = () => (
           <Offcanvas.Title id="offcanvasNavbarLabel">Menu</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          <Nav className="justify-content-center flex-grow-1 pe-3">
-            <Nav.Link className="mx-lg-2 active" href="#">Courses</Nav.Link>
+          <Nav className="navbar-nav justify-content-end flex-grow-1 pe-3">
+            <div className="dropdown">
+              <Nav.Link className="mx-lg-2 dropdown-toggle" href="#" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                Courses
+              </Nav.Link>
+              <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    SAP &raquo;
+                  </a>
+                  <ul className="dropdown-menu dropdown-submenu">
+                    <li>
+                      <a className="dropdown-item" href="#">SAP Functional  &raquo;</a>
+                      <ul className="dropdown-menu dropdown-submenu">
+                        <li>
+                          <a className="dropdown-item" href="#">SAP FICO</a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">SAP MM 2</a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">SAP SD</a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">SAP HR/HCM</a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">SAP PP</a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">SAP SUCCESSFACTOR</a>
+                        </li>
+                      </ul>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">SAP Technical &raquo;</a>
+                      <ul className="dropdown-menu dropdown-submenu">
+                        <li>
+                          <a className="dropdown-item" href="#">SAP ABAP</a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">SAP BASIS</a>
+                        </li>
+                      </ul>
+                    </li> 
+                  </ul>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    IT Courses &raquo;
+                  </a>
+                  <ul className="dropdown-menu dropdown-submenu">
+                    <li>
+                      <a className="dropdown-item" href="#">Data Science &raquo;</a>
+                      <ul className="dropdown-menu dropdown-submenu">
+                        <li>
+                            <a className="dropdown-item" href="#">Masters in Data Science </a>
+                          </li>
+                          <li>
+                            <a className="dropdown-item" href="#">Masters in Data Analytics</a>
+                          </li>
+                          <li>
+                            <a className="dropdown-item" href="#">Masters in Business Analytics</a>
+                          </li>
+                          <li>
+                            <a className="dropdown-item" href="#">Chat Gpt and AI</a>
+                          </li>
+                        </ul>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">Submenu item 2</a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">Submenu item 3 &raquo;</a>
+                      <ul className="dropdown-menu dropdown-submenu">
+                        <li>
+                          <a className="dropdown-item" href="#">Multi level 1</a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">Multi level 2</a>
+                        </li>
+                      </ul>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Submenu &raquo;
+                  </a>
+                  <ul className="dropdown-menu dropdown-submenu">
+                    <li>
+                      <a className="dropdown-item" href="#">Submenu item 1</a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">Submenu item 2</a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">Submenu item 3 &raquo;</a>
+                      <ul className="dropdown-menu dropdown-submenu">
+                        <li>
+                          <a className="dropdown-item" href="#">Multi level 1</a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">Multi level 2</a>
+                        </li>
+                      </ul>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">Submenu item 4</a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">Submenu item 5</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
             <Nav.Link className="mx-lg-2" href="#">Placements</Nav.Link>
             <Nav.Link className="mx-lg-2" href="#">Blogs</Nav.Link>
           </Nav>
         </Offcanvas.Body>
       </Navbar.Offcanvas>
-      
+
     </Container>
   </Navbar>
 );
