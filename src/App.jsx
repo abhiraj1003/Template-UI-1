9b95b2472791bc6c882023b2c36d6e26f1b56b48
abhiraj1003 <abhiraj007lol@gmail.com>
2024-07-04 08:04:17 +0000

    Pending changes exported from your codespace

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './components/Navbar';
+import Navbar from './components/Navbar';
 import HeaderCarousel from './Pages/HeaderCarousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -9,20 +9,16 @@ import Keypoints from './Pages/Keypoints';
 import PopCourses from './Pages/PopCourses';
 import FeedbackAndReviews from './Pages/FeedbacksandReviews';
 
-
 function App() {
   return (
     <div className="App">
-      <Marquee />
-      <Header />
+      <Navbar />
       <HeaderCarousel />
       <Marquee />
       <Keypoints />
       <PopCourses />
       <PlacementSection />
       <FeedbackAndReviews />
-      
-      
     </div>
   );
 }
